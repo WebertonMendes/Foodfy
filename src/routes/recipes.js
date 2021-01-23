c3800f6fa2c518bf9927df9129cab195985bc72a
WebertonMendes <weberton.mendes@hotmail.com>
2021-01-23 00:52:19 -0300

    Foodfy

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -13,8 +13,8 @@ routes.get('/', RecipesController.index) // Admin - Index Visualizar a Lista das
 routes.get('/create', RecipesController.create) // Admin - Criar uma Nova Receita
 routes.get('/:id', RecipesController.show) // Admin - Visualizar Detalhes da Receita
 routes.get('/:id/edit', ValidatorAccess.permissionRecipesRoutes, RecipesController.edit) // Admin - Editar uma Receita
-routes.post('/', multer.array('recipe-image', 5), ValidatorAccess.permissionRecipesRoutes, RecipesController.post) // Admin - Enviando Dados da Nova Receita
-routes.put('/:id', multer.array('recipe-image', 5), ValidatorAccess.permissionRecipesRoutes, RecipesController.put) // Admin - Enviando Dados Alterados na Receita
-routes.delete('/:id', ValidatorRecipe.deleteRecipe, ValidatorAccess.permissionRecipesRoutes, RecipesController.delete) // Admin - Deletando uma Receita
+routes.post('/', ValidatorAccess.permissionRecipesRoutes, multer.array('recipe-image', 5), RecipesController.post) // Admin - Enviando Dados da Nova Receita
+routes.put('/:id', ValidatorAccess.permissionRecipesRoutes, multer.array('recipe-image', 5), RecipesController.put) // Admin - Enviando Dados Alterados na Receita
+routes.delete('/:id', ValidatorAccess.permissionRecipesRoutes, ValidatorRecipe.deleteRecipe, RecipesController.delete) // Admin - Deletando uma Receita
 
 module.exports = routes
